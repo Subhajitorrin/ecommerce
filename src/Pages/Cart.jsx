7a1b89ed76b0695ab4c8314967cdb8110bc832ac
Subhajitorrin <orrin2op@gmail.com>
2024-03-26 21:42:04 +0530

    update

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,18 +1,44 @@
 import React, { useEffect, useState } from "react";
 import Cartcard from "../Components/Cartcard";
+import { RxCross2 } from "react-icons/rx";
 
 function Cart(props) {
-  const [totalPrice,setTotalPrice] = useState(0);
-  function calculateTotalPrice(){
-    let tempPrice=0
-    for(let i=0;i<props.cart.length;i++){
-      tempPrice+=props.cart[i].price*props.cart[i].quantity;
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [discout, setDiscount] = useState(0);
+  const discountAmount = 100;
+  function calculateTotalPrice() {
+    let tempPrice = 0;
+    for (let i = 0; i < props.cart.length; i++) {
+      tempPrice += props.cart[i].price * props.cart[i].quantity;
     }
-    setTotalPrice(tempPrice)
+    tempPrice=tempPrice.toFixed(2);
+    setTotalPrice(tempPrice);
+  }
+  function usePromocode() {
+    if (discout === 0) {
+      const promoCode = document.querySelector("#promocode").value;
+      const promoCross = document.querySelector(".promoCross");
+      if (promoCode === "ORRIN100" && totalPrice >= 500) {
+        setDiscount(discountAmount);
+        setTotalPrice(totalPrice - discountAmount);
+        promoCross.style.pointerEvents = "auto";
+        promoCross.style.opacity = 100;
+        document.querySelector("#promocode").disabled = true;
+      }
+    }
+  }
+  function unusePromocode() {
+    document.querySelector("#promocode").value = "";
+    const promoCross = document.querySelector(".promoCross");
+    setDiscount(0);
+    setTotalPrice(totalPrice + discountAmount);
+    promoCross.style.pointerEvents = "none";
+    promoCross.style.opacity = 0;
+    document.querySelector("#promocode").disabled = false;
   }
   useEffect(() => {
-    calculateTotalPrice()
-  }, [props.cart])
+    calculateTotalPrice();
+  }, [props.cart]);
   return (
     <div className="cartcontainer">
       <div className="left">
@@ -32,15 +58,25 @@ function Cart(props) {
       </div>
       <div className="right">
         <div className="cost">
+          <h6 className="promoheading">Use <span className="promocodebox">ORRIN100</span> promocode on first payment</h6>
           <div className="promo">
-            <input type="text" name="" id="" placeholder="Enter promo code"/>
-            <button>Submit</button>
+            <div className="promoNcross">
+              <input
+                type="text"
+                name="promocode"
+                id="promocode"
+                placeholder="Enter promo code"
+              />
+              <RxCross2 className="promoCross" onClick={unusePromocode} />
+            </div>
+
+            <button onClick={usePromocode}>Submit</button>
           </div>
           <div className="innercost">
             <h4>Shopping Cost:</h4> <h4>${totalPrice}</h4>
           </div>
           <div className="innercost">
-            <h4>Discount:</h4> <h4>-$0</h4>
+            <h4>Discount:</h4> <h4>-${discout}</h4>
           </div>
           <div className="innercost">
             <h3>Total Cost:</h3> <h3>${totalPrice}</h3>
